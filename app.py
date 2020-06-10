dde03ae0f2836b3e93b80c5a25c16dde0fe70f31
thearjun <mailarjunadhikari@gmail.com>
2020-06-11 02:01:39 +0545

    Change context

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ from scrape import scrape_vegetable_price
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecret_key'
 
-@app.route('price_api/')
+@app.route('/')
 def api():
     
-    context = scrape_vegetable_price(lang='NE', 
-                                    date='06/10/2020', 
+    context = scrape_vegetable_price(lang='EN', 
+                                    date='', 
                                     pricetype='W')
     return jsonify(context)
 
