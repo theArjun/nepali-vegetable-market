19637bb1610fffca37b03e279c3cb7d1e4856849
thearjun <mailarjunadhikari@gmail.com>
2020-06-11 03:17:49 +0545

    Make API Data save in 3 hours interval

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import json
+import time
+from threading import Thread
 
 from flask import Flask, jsonify
 
@@ -6,13 +9,35 @@ from scrape import scrape_vegetable_price
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecret_key'
 
+def background_task():
+    """ Saving API details in every 3 hours """
+    while True:
+        context = scrape_vegetable_price(lang='EN', 
+                                    date='', 
+                                    pricetype='W',
+                                    last_n_days=10)
+
+        with open('price_data.json', 'w') as price_data:
+            price_data.write(json.dumps(context))
+        time.sleep(3*60*60)
+
+Thread(target=background_task, daemon=True).start()
+
 @app.route('/')
 def api():
-    
-    context = scrape_vegetable_price(lang='EN', 
-                                    date='', 
-                                    pricetype='W')
-    return jsonify(context)
+    """ API View for fruits and vegetables price """
+
+    try:
+        price_data = open('price_data.json', 'r').read()
+    except FileNotFoundError:
+        context = scrape_vegetable_price(lang='EN', 
+                                        date='', 
+                                        pricetype='W',
+                                        last_n_days=10)
+        price_data = open('price_data.json', 'w').write(json.dumps(context))
+
+
+    return jsonify(json.loads(price_data))
 
 if __name__ == "__main__":
     app.run()
