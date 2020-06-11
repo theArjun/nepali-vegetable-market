4345098d6195e76e0dd015d0d1a58c6cd6bb6ff2
thearjun <mailarjunadhikari@gmail.com>
2020-06-11 23:05:22 +0545

    Updates

---
+++ precache-manifest.6b83d603ad31e1e0b350268655a00f90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88d3f272951c611f6bdcb7431c66a52c",
+    "url": "/nepali-vegetable-market/index.html"
+  },
+  {
+    "revision": "cebd4b10bf202beece77",
+    "url": "/nepali-vegetable-market/static/css/main.a06da389.chunk.css"
+  },
+  {
+    "revision": "27885922dabe875d6ec7",
+    "url": "/nepali-vegetable-market/static/js/2.e482a87b.chunk.js"
+  },
+  {
+    "revision": "bbe2dbbdaea8525fcc46934c5c58f863",
+    "url": "/nepali-vegetable-market/static/js/2.e482a87b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cebd4b10bf202beece77",
+    "url": "/nepali-vegetable-market/static/js/main.f481ffb6.chunk.js"
+  },
+  {
+    "revision": "12e5e5ff4f061603e05f",
+    "url": "/nepali-vegetable-market/static/js/runtime-main.3b2c677d.js"
+  }
+]);
