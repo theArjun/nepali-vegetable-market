7366059a39acc4a2042f5a4ba7ad58e94e8ffe7b
thearjun <mailarjunadhikari@gmail.com>
2020-06-15 11:46:06 +0545

    Add function for writing JSON files

--- app.py
+++ app.py
@@ -12,16 +12,19 @@ app.config['SECRET_KEY'] = 'mysecret_key'
 
 CORS(app)
 
-def background_task():
-    """ Saving API details in every 3 hours """
-    while True:
-        context = scrape_vegetable_price(lang='EN', 
+def write_on_file():
+    context = scrape_vegetable_price(lang='EN', 
                                     date='', 
                                     pricetype='W',
                                     last_n_days=10)
 
-        with open('price_data.json', 'w') as price_data:
-            price_data.write(json.dumps(context))
+    with open('price_data.json', 'w') as price_data:
+        price_data.write(json.dumps(context))
+
+def background_task():
+    """ Saving API details in every 3 hours """
+    while True:
+        write_on_file()
         time.sleep(3*60*60)
 
 Thread(target=background_task, daemon=True).start()
@@ -33,12 +36,7 @@ def api():
     try:
         price_data = open('price_data.json', 'r').read()
     except FileNotFoundError:
-        context = scrape_vegetable_price(lang='EN', 
-                                        date='', 
-                                        pricetype='W',
-                                        last_n_days=10)
-        price_data = open('price_data.json', 'w').write(json.dumps(context))
-
+        write_on_file()
 
     return jsonify(json.loads(price_data))
 
